503d6cd9a17653ff28702db0034e2fb9bccc6db1
axcm19 <afonsoxcm@gmail.com>
2024-10-25 14:21:20 +0100

    begin frontend

--- PackMyBag-frontend/src/App.vue
+++ PackMyBag-frontend/src/App.vue
@@ -1,26 +1,25 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
+
+
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="background-container">
+    <div id="app">
+      <HomeView></HomeView>
     </div>
-  </header>
+  </div>
 
-  <RouterView />
 </template>
 
-<style scoped>
+<!--
+<style>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -56,7 +55,7 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 2024px) {
   header {
     display: flex;
     place-items: center;
@@ -83,3 +82,5 @@ nav a:first-of-type {
   }
 }
 </style>
+
+-->
