3fbc226f94498bf67c6daffb74c7543e2419fd96
axcm19 <afonsoxcm@gmail.com>
2024-10-29 11:55:48 +0000

    stylist page

---
+++ PackMyBag-frontend/src/components/StylistIndividualView.vue
@@ -0,0 +1,50 @@
+<template>
+           
+  	<div class="stylistcomponent">
+    		<div class="stylistcomponent-child">
+    		</div>
+    		<div class="image-8-wrapper">
+      			<img class="image-8-icon" alt="" src="/StylistsIMG/image 8.png">
+      			
+    		</div>
+    		<div class="stylistcomponent-item">
+    		</div>
+    		<div class="gonalo-vale">Gonçalo Vale</div>
+    		<div class="stylistcomponent-inner">
+      			<div class="frame-parent">
+        			<Rating id="avrgestrelas" :modelValue="stylistrate" @update:modelValue="stylistrate = $event" readonly/>	
+      			</div>
+    		</div>
+    		<div class="an-expert-in-container">
+      			<p class="an-expert-in">An expert in streetwear, I bring a personalized touch to every wardrobe. With a sharp eye for </p>
+      			<p class="an-expert-in">trends and a passion for curating looks that reflect individual style, I help clients feel confident and effortlessly chic. Whether it’s for personal</p>
+      			<p class="an-expert-in"> shopping, special events, or editorial work, my goal is to make fashion both fun and accessible.</p>
+    		</div>
+			<button class="vector-icon" @click="goToForms()"></button>
+    		
+  	</div>
+
+
+</template>
+
+<script>
+import Rating from 'primevue/rating';
+export default {
+	components:{
+		Rating
+	},
+	data(){
+		return{
+			stylistrate:3
+		}
+	},
+	methods:{
+		goToForms(){
+			this.$router.push({ path: '/about' });
+		}
+	}
+}
+</script>
+
+
+<style scoped src="../assets/stylistindividual.css"></style>
--- PackMyBag-frontend/src/views/StylistsView.vue
+++ PackMyBag-frontend/src/views/StylistsView.vue
@@ -2,54 +2,15 @@
     <main>
         <NavBarComponent></NavBarComponent>
         
+            
             <div class="stylistspage">
                     <div class="stylistspage-child">
                     </div>
-                    <div class="image-8-wrapper">
-                        <img class="image-8-icon" alt="" src="/StylistsIMG/image 8.png">
-                        
-                    </div>
-                    <div class="stylistspage-item">
-                    </div>
-                    <div class="stylistspage-inner">
-                    </div>
-                    <div class="gonalo-vale">Gonçalo Vale</div>
-                    <div class="frame-div">
-                        <div class="frame-parent">
-                                <div class="star-parent">
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                </div>
-                                <div class="reviews">4 (154 reviews)</div>
-                        </div>
-                    </div>
-                    <div class="orlando-belo">Orlando Belo</div>
-                    <div class="stylistspage-inner1">
-                        <div class="frame-parent">
-                                <div class="star-parent">
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                    <img class="star-icon" alt="" src="/StylistsIMG/Star.png">
-                                    
-                                </div>
-                                <div class="reviews">5 (11 reviews)</div>
-                        </div>
-                    </div>
                     <div class="stylists">Stylists</div>
+
+                    <div v-for="s in 2">
+						<StylistIndividualView></StylistIndividualView>
+					</div>
                     
                     <div class="parent">
                         <div class="div1">1</div>
@@ -62,36 +23,22 @@
                         <div class="rectangle-parent">
                                 <div class="group-child">
                                 </div>
-                                <!-- <img class="group-item" alt="" src="Vector 9.svg"> -->
+                                <img class="group-item" alt="" src="/CatalogueIMG/nextbtn.png">
                                 
                         </div>
                     </div>
-                    <div class="an-expert-in-container">
-                        <p class="an-expert-in">An expert in streetwear, I bring a personalized touch to every wardrobe. With a sharp eye for </p>
-                        <p class="an-expert-in">trends and a passion for curating looks that reflect individual style, I help clients feel confident and effortlessly chic. Whether it’s for personal</p>
-                        <p class="an-expert-in"> shopping, special events, or editorial work, my goal is to make fashion both fun and accessible.</p>
-                    </div>
-                    <div class="rectangle-div">
-                    </div>
-                    <div class="specializing-in-vintage-inspir-container">
-                        <p class="an-expert-in">Specializing in vintage-inspired looks, I transform wardrobes with a focus on effortless style and individual expression. I work closely with clients </p>
-                        <p class="an-expert-in">to create polished, versatile outfits that suit their lifestyle and personality. Whether it's for everyday wear or a special occasion, my mission is to </p>
-                        <p class="an-expert-in">make getting dressed simple, stylish, and fun.</p>
-                    </div>
-                    <img class="image-16-icon" alt="" src="/StylistsIMG/image 16.png">
-                    
                     <div class="genderfilter">
-                        <img class="background-icon" alt="" src="/StylistsIMG/◼️ Background.svg">
-                        
-                        <div class="text">Filter</div>
-                        <div class="iconcontrolexpand-more">
-                        </div>
+
+                        <div class="text">Gender</div>
+                        <img class="iconcontrolexpand-more" alt="" src="/SignUpIMG/Icon/control/expand_more.svg">
+                            <VueSelect
+                                v-model="selectedOption"
+                                :options="typeOptions" placeholder="Gender"
+                            />
                     </div>
-                    <img class="vector-icon" alt="" src="/StylistsIMG/Vector.svg" id="vector">
-                    
-                    <img class="vector-icon1" alt="" src="/StylistsIMG/Vector.svg" id="vector1">
-                    
             </div>
+  	
+  	
   	
         <FooterComponent></FooterComponent>
     </main>
@@ -101,14 +48,33 @@
 
 import NavBarComponent from '@/components/NavBarComponent.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
+import StylistIndividualView from '@/components/StylistIndividualView.vue';
+import VueSelect from "vue3-select-component";
+
 export default {
+
 components:{
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    StylistIndividualView,
+    VueSelect
+},
+
+data(){
+    return{
+        value: [0,1000],
+        selectedOption:'',
+        typeOptions: [{ label: 'Male', value: 'Male' },{ label: 'Female', value: 'Female' }],
+        items: [],
+        filtered: [],
+        current_page:0
+    }
 }
+
 }
 
 </script>
 <style lang="css" scoped>
 @import '../assets/stylists.css';
 </style>
+<style src="@vueform/slider/themes/default.css"></style>
