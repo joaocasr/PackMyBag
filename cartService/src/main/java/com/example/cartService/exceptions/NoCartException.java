49f7c83c45c692d80c26eab9e7c15e66337ac374
André Alves <nseiporamor@gmail.com>
2024-10-30 16:51:32 +0000

    Forgot this file

--- cartService/src/main/java/com/example/cartService/exceptions/NoCartException.java
+++ cartService/src/main/java/com/example/cartService/exceptions/NoCartException.java
@@ -1,1 +1,7 @@
- 
+package com.example.cartService.exceptions;
+
+public class NoCartException extends RuntimeException {
+    public NoCartException(String message) {
+        super(message);
+    }
+} 
