8d9e995be11ae2979dfd4691f6f1b27ac1063e65
axcm19 <afonsoxcm@gmail.com>
2024-11-20 17:54:16 +0000

    started routes for recomendacoes

---
+++ API-GATEWAY/microservices/recomendacoesService.js
@@ -0,0 +1,135 @@
+var axios = require('axios')
+var ap = process.env.RECOMENDACOES_SERVICE_URL || "http://localhost:8088/api/recomendacoes"
+
+module.exports.getCartItems = (username) => {
+    return axios.get(`${ap}/${username}`).then(resp => {
+        return resp.data;
+    }).catch(err => {
+        throw { error: err.response.data };
+    });
+}
+
+module.exports.addToCart = (itemBody) => {
+    return axios.post(`${ap}/addItem`,
+        itemBody,
+        {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+            }
+        }
+    ).then(resp => {
+        return resp.data;
+    }).catch(err => {
+        if (err.response) {
+            throw { error: err.response.data };
+        } else {
+            throw err;
+        }
+    });
+}
+
+module.exports.getpayment = async (codigo) => {
+    try{
+        let r = await axios.get(`${ap}/transaction/${codigo}`)
+        return r.data;
+    }catch(err){
+        if (err.response) {
+            throw { error: err.response.data };
+        } else {
+            throw err;
+        }
+    }
+}
+
+module.exports.changePaymentStatus = (itemBody) => {
+    return axios.post(`${ap}/changePaymentStatus`,
+        itemBody,
+        {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+            }
+        }
+    ).then(resp => {
+        return resp.data;
+    }).catch(err => {
+        if (err.response) {
+            throw { error: err.response.data };
+        } else {
+            throw err;
+        }
+    });
+}
+
+module.exports.removeFromCart = (itemBody) => {
+    return axios.post(`${ap}/removeItem`,
+        itemBody,
+        {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+            }
+        }
+    ).then(resp => {
+        return resp.data;
+    }).catch(err => {
+        if (err.response) {
+            throw { error: err.response.data };
+        } else {
+            throw err;
+        }
+    });
+}
+
+module.exports.getEncomendasByLojaID = (lojaid) => {
+    return axios.get(`${ap}/loja/${lojaid}`).then(resp => {
+        return resp.data;
+    }).catch(err => {
+        if (err.response) {
+            throw { error: err.response.data };
+        } else {
+            throw err;
+        }
+    });
+}
+
+
+module.exports.getTransactions = (username) => {
+    return axios.get(`${ap}/transactions/${username}`).then(resp => {
+        return resp.data;
+    }).catch(err => {
+        throw { error: err.response.data };
+    });
+}
+
+
+module.exports.createPaymentForms = async (itemBody) => {
+    try{
+        let r = await axios.post(`${ap}/createPayment`,
+                itemBody,
+                {
+                    headers: {
+                        'Content-Type': 'application/json;charset=UTF-8',
+                    }
+                })
+        return r.data;
+    }catch(err){
+        return err;
+    } 
+        
+}
+
+
+module.exports.createPayment = async (itemBody) => {
+    try{
+        let r = await axios.post(`${ap}/createPaymentCartClean`,
+                itemBody,
+                {
+                    headers: {
+                        'Content-Type': 'application/json;charset=UTF-8',
+                    }
+                })
+        return r.data;
+    }catch(err){
+        return err;
+    } 
+        
+}
---
+++ API-GATEWAY/routes/recomendacoesRouter.js
@@ -0,0 +1,181 @@
+var express = require('express');
+var router = express.Router();
+const validate = require('../middleware/index')
+const recomendacoesService = require('../microservices/recomendacoesService')
+
+/* Get Cart Items */
+router.get("/:username", function(req, res, next) {
+  cartService.getCartItems(req.params.username).then(resp => {
+    res.jsonp(resp);
+  }).catch(err => {
+    res.status(400).jsonp(err);
+  });
+});
+
+/* Add Item to Cart */
+router.post("/addItem", validate.verifyToken, function(req, res, next) {
+  cartService.addToCart(req.body).then(resp => {
+    res.jsonp(resp);
+  }).catch(err => {
+    res.status(403).jsonp(err);
+  });
+});
+
+/* Remove Item from Cart */
+router.post("/removeItem", function(req, res, next) {
+  cartService.removeFromCart(req.body).then(resp => {
+    res.jsonp(resp);
+  }).catch(err => {
+    res.status(400).jsonp(err);
+  });
+});
+
+/* Clear Cart */
+router.post("/clearCart", function(req, res, next) {
+  cartService.clearCart(req.body).then(resp => {
+    res.jsonp(resp);
+  }).catch(err => {
+    res.status(400).jsonp(err);
+  });
+});
+
+/* Get User transactions */
+router.get("/transactions/:username", function(req, res, next) {
+  cartService.getTransactions(req.params.username).then(resp => {
+    res.jsonp(resp);
+  }).catch(err => {
+    res.status(404).jsonp(err);
+  });
+});
+
+
+/* just payment */
+router.post("/newpayment", validate.verifyToken,  async function(req,res,next){
+  try{
+
+    const username = req.body.username;
+    const codigo = "FOR"+username.toUpperCase()+Date.now();
+    const payment = {
+                      "username":username,
+                      "codigo": codigo,
+                      "total":req.body.total,
+                      "modoPagamento":req.body.modoPagamento,
+                      "status": "PENDING",
+                      "dataGeracao":req.body.dataGeracao
+                    }
+    
+      let r = await cartService.createPaymentForms(payment); // create payment
+      //falta adicionar o pedido no estilista
+      res.status(200).jsonp(r);
+  }catch(err){
+    res.status(400).jsonp(err);
+
+  }
+})
+
+
+/* rota para alugar os items do cesto */
+router.post("/order", validate.verifyToken, async function(req,res,next){
+  const itensObj = req.body.itensObj;
+  const username = req.body.username;
+  try{
+    console.log(itensObj);
+    const result = await catalogoService.verifyAvailability(itensObj);
+    if(result.status==200){
+          console.log("passou")
+          console.log(username)
+    
+          const codigo = "ENC"+username.toUpperCase()+Date.now();
+          const payment = {
+                            "username":username,
+                            "codigo": codigo,
+                            "localEntrega":req.body.localEntrega,
+                            "inicioAluguer":req.body.inicioAluguer,
+                            "fimAluguer":req.body.fimAluguer,
+                            "total":req.body.total,
+                            "modoPagamento":req.body.modoPagamento,
+                            "dataGeracao":req.body.dataGeracao,
+                            "status": "PENDING",
+                            "items":itensObj.itens
+                          }
+          console.log(payment);
+          try{
+            let r = await cartService.createPayment(payment); // create payment
+            res.status(200).jsonp(r);
+          }catch(err){
+            res.status(400).jsonp(err);
+          }            
+    }
+  }catch(err){
+    res.status(400).jsonp(err);
+
+  }
+})
+
+
+/* actually pay */
+router.post("/pay", validate.verifyToken, async function(req,res,next){
+  console.log("let's pay");
+  try{
+
+      if(req.body.ptype==="FORM"){
+        //alterar status desse pedido para payed com o codigo no microservico de recomendacoes | POST
+      }
+      if(req.body.ptype==="CART"){
+        try{
+          const statusPayment = {
+                "codigo":req.body.codigo,
+                "username":req.body.username,
+                "status":"PAYED"
+          }  
+          let resp1 = await cartService.changePaymentStatus(statusPayment);
+          console.log("resp 1")
+          console.log(resp1);
+          if(resp1==="Payment status updated successfully!"){ //get dos items associados ao pagamento
+
+            try{  
+              
+                    let resp2 = await cartService.getpayment(req.body.codigo); 
+                    console.log("resp 2");
+                    console.log(resp2);         
+                    let paymentInfoItems = resp2;
+
+                    console.log("a imprimir a nova encomenda")
+                    const encomenda = {
+                      "codigoEncomenda":req.body.codigo,
+                      "dataEntrega": paymentInfoItems.inicioAluguer,
+                      "dataDevolucao": paymentInfoItems.fimAluguer,
+                      "localEntrega": paymentInfoItems.localEntrega,
+                      "status": "Pago",
+                      "clienteUsername": req.body.username,
+                      "clienteNome": req.body.nome,
+                      "clienteEmail": req.body.email,
+                      "itens": paymentInfoItems.items,
+                      "preco": req.body.total,
+                      "taxaEntrega": 6.0
+                    }
+                    console.log(encomenda);
+                    try{
+                      let resp3 = await encomendaService.createEncomenda(encomenda);
+                      res.jsonp(resp3.data);
+                    }catch(err){
+                      console.log(err);
+                    }
+            }catch(err){
+              console.log(err);
+            }
+          }
+        }catch(err){
+          console.log(err);
+        }
+      }
+  }catch(err){
+    console.log(err);
+    res.status(400).jsonp(err);
+
+  }
+})
+
+
+
+module.exports = router;
